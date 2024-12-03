e34866f36029ec648beb7bf13c1c8673abcab13d
jilkujiptha <mpjiptha@gmail.com>
2024-12-03 18:00:43 +0530

    music

---
+++ musicapp/lib/MusicPage.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MusicPage extends StatefulWidget {
+  const MusicPage({super.key});
+
+  @override
+  State<MusicPage> createState() => _MusicPageState();
+}
+
+class _MusicPageState extends State<MusicPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.widgets_outlined,
+            color: Colors.white,
+            size: 35,
+          ),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              size: 35,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color.fromARGB(255, 30, 11, 63),
+              const Color.fromARGB(193, 13, 1, 34),
+            ],
+            end: Alignment.topCenter,
+            begin: Alignment.bottomCenter,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ musicapp/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:musicapp/MusicPage.dart';
+void main(){
+  runApp(MaterialApp(home: MusicPage(),));
+}
